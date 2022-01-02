36a9a8abbf05944619abffb69b552aa0b6a8d83b
theracerx <60013361+theracerx@users.noreply.github.com>
2022-01-02 23:36:44 +0800

    sdafa

---
+++ config.js
@@ -0,0 +1,38 @@
+//slideshow link
+const slideshow;
+
+const InspectViewer=[//vid,link,img,audio
+    {pos:0}
+]
+const PanoViewer=[
+    {pos:0}
+]
+const MdlViewer=[
+    {pos:0}
+]
+const Subtitles=[
+    {pos:0}
+]
+const CarouselViewer=[
+    {pos:0}
+]
+const CustomFunc=[
+    {name:"myFunction",func:()=>{}}
+]
+const ExternalAudio=[
+    {pos:0}
+]
+
+/*  Callables
+display("Inspect",0,0)
+display("Panorama",0,0)
+display("Mdl",0,0)
+display("Subtitle",0,0)
+display("Carousel",0,0)
+display("Timer",startT,endT)
+
+call("Modal")
+playAudio("")
+playSfxAudio("")
+playBgAudio("")
+*/
